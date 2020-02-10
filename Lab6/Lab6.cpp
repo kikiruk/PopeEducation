9560e775cfa68284b32fcc91f740159a7fee3282
BuildTools <kikiruk123@naver.com>
2020-02-10 23:10:18 +0900

    실습 6

--- Lab6/Lab6.cpp
+++ Lab6/Lab6.cpp
@@ -1,4 +1,5 @@
 #include<climits> 
+#include<climits> 
 #include "Lab6.h"
 
 namespace lab6
@@ -16,7 +17,7 @@ namespace lab6
 		return 0;
 	}
 
-	int Min(const std::vector<int>& v)
+	int Min(const std::vector<int> & v)
 	{
 		int minNum = INT_MAX;
 
@@ -26,7 +27,7 @@ namespace lab6
 		return 0;
 	}
 
-	int Max(const std::vector<int>& v)
+	int Max(const std::vector<int> & v)
 	{
 		int maxNum = -INT_MAX;
 
@@ -36,19 +37,69 @@ namespace lab6
 		return 0;
 	}
 
-	float Average(const std::vector<int>& v)
+	float Average(const std::vector<int> & v)
 	{
 		return Sum(v) / v.size();
 	}
 
-	int NumberWithMaxOccurrence(const std::vector<int>& v)
+	int NumberWithMaxOccurrence(const std::vector<int> & v)
 	{
-		return 0;
+		std::vector<int> tmp(v);
+		SortDescending(tmp);
+
+		int answer = v[0];
+		int maxCount = 1;
+		int numberNow = v[0];
+		int numberCount = 1;
+
+		for (int i = 1; i < tmp.size(); i++)
+		{
+			if (numberNow == v[i])
+				numberCount++;
+			else
+			{
+				if (maxCount < numberCount)
+				{
+					maxCount = numberCount;
+					answer = numberNow;
+				}
+
+				numberCount = 1;
+				numberNow = v[i];
+			}
+		}
+
+		return answer;
 	}
 
-	void SortDescending(std::vector<int>& v)
+	void SortDescending(std::vector<int> & v)
 	{
-
+		quickSort(0, v.size() - 1, v);
 	}
 
+	void quickSort(int left, int right, std::vector<int> & v)
+	{
+		int pivot = right;
+		int j = pivot;
+		int i = right - 1;
+		int tmp;
+
+		if (left < right) {
+			for (; i >= left; i--) {
+				if (v[i] < v[pivot]) {
+					j--;
+					tmp = v[j];
+					v[j] = v[i];
+					v[i] = tmp;
+				}
+			}
+			tmp = v[right];
+			v[right] = v[j];
+			v[j] = tmp;
+			pivot = j;
+
+			quickSort(left, pivot - 1, v);
+			quickSort(pivot + 1, right, v);
+		}
+	}
 }
--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -1,4 +1,23 @@
+#include <vector>
+#include <iostream>
+#include "Lab6.h"
+
+using namespace std;
+
 int main()
 {
+	std::vector<int> v;
+	v.push_back(4);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(12);
+	v.push_back(7);
+	v.push_back(11);
 
+	cout << lab6::NumberWithMaxOccurrence(v) << endl; // 4
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
 }
