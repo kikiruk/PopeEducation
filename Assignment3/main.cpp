97b64a3198fee9954de01aedd1aac4e58b5443d1
BuildTools <kikiruk123@naver.com>
2020-03-01 15:20:53 +0900

    과제 3

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -10,114 +10,34 @@ using namespace std;
 int main()
 {
 	SmartStack<long> ss;
-
 	ss.Push(1);
 
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Push(2);
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Push(3);
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Push(4);
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Push(5);
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Push(6);
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Push(7);
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Push(8);
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Push(9);
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Push(10);
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
+	cout << ss.GetMin() << " < " << ss.GetMax() << endl;
+	ss.Push(1);
+	cout << ss.GetMin() << " < " << ss.GetMax() << endl;
+	ss.Push(1);
+	cout << ss.GetMin() << " < " << ss.GetMax() << endl;
+	ss.Push(1);
+	cout << ss.GetMin() << " < " << ss.GetMax() << endl;
+	ss.Push(1);
+	ss.Push(1);
+	ss.Push(1);
+	ss.Push(1);
+	ss.Push(1);
+	ss.Push(1);
+	ss.Push(1);
+	ss.Push(1);
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
 	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-	ss.Pop();
-
-	cout << "Min : " << ss.GetMin() << ", Max : " << ss.GetMax() << endl;
-
-
+	cout << ss.GetMin() << " < " << ss.GetMax() << endl;
 }
