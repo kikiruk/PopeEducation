eab559b0a2739663de58764523cdc3145ced59a1
seagull <33773761+kikiruk@users.noreply.github.com>
2020-03-14 17:08:43 +0900

    실습 8

--- Lab8/FixedBoolVector.h
+++ Lab8/FixedBoolVector.h
@@ -1,113 +1,6 @@
 #pragma once
 
-namespace Squared
-{
-	uint32_t pow_2(uint32_t num)
-	{
-		switch (num)
-		{
-		case 0:
-			return 1;
-			break;
-		case 1:
-			return 2;
-			break;
-		case 2:
-			return 4;
-			break;
-		case 3:
-			return 8;
-			break;
-		case 4:
-			return 16;
-			break;
-		case 5:
-			return 32;
-			break;
-		case 6:
-			return 64;
-			break;
-		case 7:
-			return 128;
-			break;
-		case 8:
-			return 256;
-			break;
-		case 9:
-			return 512;
-			break;
-		case 10:
-			return 1024;
-			break;
-		case 11:
-			return 2048;
-			break;
-		case 12:
-			return 4096;
-			break;
-		case 13:
-			return 8192;
-			break;
-		case 14:
-			return 16384;
-			break;
-		case 15:
-			return 32768;
-			break;
-		case 16:
-			return 65536;
-			break;
-		case 17:
-			return 131072;
-			break;
-		case 18:
-			return 262144;
-			break;
-		case 19:
-			return 524288;
-			break;
-		case 20:
-			return 1048576;
-			break;
-		case 21:
-			return 2097152;
-			break;
-		case 22:
-			return 4194304;
-			break;
-		case 23:
-			return 8388608;
-			break;
-		case 24:
-			return 16777216;
-			break;
-		case 25:
-			return 33554432;
-			break;
-		case 26:
-			return 67108864;
-			break;
-		case 27:
-			return 134217728;
-			break;
-		case 28:
-			return 268435456;
-			break;
-		case 29:
-			return 536870912;
-			break;
-		case 30:
-			return 1073741824;
-			break;
-		case 31:
-			return 2147483648;
-			break;
-		default:
-			return -1;
-			break;
-		}
-	}
-}
+uint32_t squared(uint32_t num);
 
 namespace lab8
 {
@@ -166,9 +59,9 @@ namespace lab8
 			return false;
 
 		if(data == true)
-			mArr[mSize / 32] |= Squared::pow_2(mSize % 32);
+			mArr[mSize / 32] |= squared(mSize % 32);
 		else
-			mArr[mSize / 32] &= ~Squared::pow_2(mSize % 32);
+			mArr[mSize / 32] &= ~squared(mSize % 32);
 
 		++mSize;
 
@@ -180,14 +73,14 @@ namespace lab8
 	{
 		for (uint32_t i = 0; i < mSize; i++)
 		{
-			if (((mArr[i / 32] | Squared::pow_2(i)) == mArr[i / 32]) == data)
+			if (((mArr[i / 32] | squared(i)) == mArr[i / 32]) == data)
 			{
 				for (uint32_t j = i + 1; j < mSize; j++)
 				{
-					if ((mArr[j / 32] | Squared::pow_2(j % 32)) == mArr[j / 32])
-						mArr[(j - 1) / 32] |= Squared::pow_2((j - 1) % 32);
+					if ((mArr[j / 32] | squared(j % 32)) == mArr[j / 32])
+						mArr[(j - 1) / 32] |= squared((j - 1) % 32);
 					else
-						mArr[(j - 1) / 32] &= ~Squared::pow_2((j - 1) % 32);
+						mArr[(j - 1) / 32] &= ~squared((j - 1) % 32);
 				}
 		
 				--mSize;
@@ -201,13 +94,13 @@ namespace lab8
 	template<size_t N>
 	inline const bool FixedVector<bool, N>::Get(const size_t index) const
 	{
-		return ((mArr[mSize / 32] | Squared::pow_2(index % 32)) == mArr[mSize / 32]);
+		return ((mArr[mSize / 32] | squared(index % 32)) == mArr[mSize / 32]);
 	}
 	
 	template<size_t N>
 	inline bool FixedVector<bool, N>::operator[](const size_t index)
 	{
-		return ((mArr[mSize / 32] | Squared::pow_2(index % 32)) == mArr[mSize / 32]);
+		return ((mArr[mSize / 32] | squared(index % 32)) == mArr[mSize / 32]);
 	}
 	
 	template<size_t N>
@@ -215,7 +108,7 @@ namespace lab8
 	{
 		for (uint32_t i = 0; i < mSize; i++)
 		{
-			if (((mArr[i / 32] | Squared::pow_2(i % 32)) == mArr[i / 32]) == data)
+			if (((mArr[i / 32] | squared(i % 32)) == mArr[i / 32]) == data)
 				return i;
 		}
 
@@ -235,5 +128,109 @@ namespace lab8
 	}
 }
 
-
+uint32_t squared(uint32_t num)
+{
+	switch (num)
+	{
+	case 0:
+		return 1;
+		break;
+	case 1:
+		return 2;
+		break;
+	case 2:
+		return 4;
+		break;
+	case 3:
+		return 8;
+		break;
+	case 4:
+		return 16;
+		break;
+	case 5:
+		return 32;
+		break;
+	case 6:
+		return 64;
+		break;
+	case 7:
+		return 128;
+		break;
+	case 8:
+		return 256;
+		break;
+	case 9:
+		return 512;
+		break;
+	case 10:
+		return 1024;
+		break;
+	case 11:
+		return 2048;
+		break;
+	case 12:
+		return 4096;
+		break;
+	case 13:
+		return 8192;
+		break;
+	case 14:
+		return 16384;
+		break;
+	case 15:
+		return 32768;
+		break;
+	case 16:
+		return 65536;
+		break;
+	case 17:
+		return 131072;
+		break;
+	case 18:
+		return 262144;
+		break;
+	case 19:
+		return 524288;
+		break;
+	case 20:
+		return 1048576;
+		break;
+	case 21:
+		return 2097152;
+		break;
+	case 22:
+		return 4194304;
+		break;
+	case 23:
+		return 8388608;
+		break;
+	case 24:
+		return 16777216;
+		break;
+	case 25:
+		return 33554432;
+		break;
+	case 26:
+		return 67108864;
+		break;
+	case 27:
+		return 134217728;
+		break;
+	case 28:
+		return 268435456;
+		break;
+	case 29:
+		return 536870912;
+		break;
+	case 30:
+		return 1073741824;
+		break;
+	case 31:
+		return 2147483648;
+		break;
+	default:
+		return -1;
+		break;
+	}
+}
 
